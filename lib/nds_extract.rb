fffee96379d66cc1924b0f019fecc214b211c566
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-23 12:25:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 require 'directors_database'
 def directors_totals(nds)
-  result = {}
+  RESULT = {}
   director_index = 0
   while director_index < nds.length do
     cumulative = 0
@@ -9,16 +9,16 @@ def directors_totals(nds)
     while title_index < nds[director_index][:movies].length do
       cumulative += nds[director_index][:movies][title_index][:worldwide_gross]
       title_index += 1
-      result[director_name] = cumulative
+      RESULT[director_name] = cumulative
     end
     director_index += 1
   end
-  p result
+  p RESULT
 end
 
 def gross_for_director(director_data)
 #  first_director_hash = Spielberg total = currently "nil"
-  p result
+  p RESULT
 #first_director_name = directors_database.first.values.first
 #first_director_hash = directors_database.find{ |x| x[:name] == first_director_name }
 end

6939b531c01731ec0390e4aa81b877c94bfbe740
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-23 12:33:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,7 +19,7 @@ end
 def gross_for_director(director_data)
 #  first_director_hash = Spielberg total = currently "nil"
 #  p directors_totals(directors_database)["Stephen Spielberg"]
-  p directors_totals(directors_database)[name]
+p directors_totals(directors_database)[first_director_name]
 #first_director_name = directors_database.first.values.first
 #first_director_hash = directors_database.find{ |x| x[:name] == first_director_name }
 end
